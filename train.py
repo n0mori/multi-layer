5948c372b1b30f82cfba99465abb4c530dcc577d
n0mori <nick33omori@gmail.com>
2019-11-19 00:02:36 -0300

    sucks

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ def eval_network(entrada, weights, biases, fn):
     result = entrada
     for w, b in zip(weights, biases):
         result = eval_layer(result, w, b, fn)
-        hs.append(result)
+        hs.append(result[np.newaxis])
     return hs
 
 
@@ -28,8 +28,9 @@ def sigma_l(d, y):
     r = (2 * y - 2 * d) * (y * (1 - y))
     return r
 
-def sigma_lminus(wl1, sl, hl, y):
-    r = (np.transpose(np.copy(wl1)) * sl) * (hl * (1 - hl))
+def sigma_lminus(wl1, sl, hl):
+    print(wl1.shape, sl.T.shape)
+    r = np.matmul(wl1, sl.T) * (hl * (1 - hl))
     return r
 
 
@@ -50,15 +51,17 @@ def train(dataset, arch, dims, reps):
         x, d = ds[idx], labels[idx]
 
         sigmas = [None for i in range(len(h))]
+        print(len(h), len(sigmas))
 
         for l in reversed(list(range(1, len(h)))):
             if l == len(h) - 1:
                 sigmas[l] = sigma_l(d, h[l])
             else:
-                sigmas[l] = sigma_lminus(thetas[l+1], sigmas[l+1], h[l], d)
-        
-        thetas[l] = thetas[l] - lr * sigmas[l] * np.transpose(np.copy(h[l-1]))
-        biases[l] = biases[l] - lr * sigmas[l]
+                sigmas[l] = sigma_lminus(thetas[l], sigmas[l+1], h[l])
+
+            print(l)
+            thetas[l-1] = thetas[l-1] - (lr * np.matmul(h[l-1].T, sigmas[l])).T
+            biases[l-1] = biases[l-1] - lr * sigmas[l]
 
     print()
     return thetas, biases
@@ -86,12 +89,31 @@ def train(dataset, arch, dims, reps):
 
 #         i += 1
 
+def test(layers, biases, testset):
+    ds = testset[:, :784]
+    labels = testset[:, 784:]
+
+    erros = 0
+    acertos = 0
+
+    for x, l in zip(ds, labels):
+        hs = eval_network(x, layers, biases, sigmoid)
+
+        if np.argmax(hs[-1]) == np.argmax(l):
+            acertos += 1
+        else:
+            erros += 1
+
+    print(erros, acertos)
+
 
 def main():
-    dataset = np.genfromtxt(f'train.csv', delimiter=',', skip_header=2)
+    dataset = np.genfromtxt(f'mnist_train.csv', delimiter=',', skip_header=2)
+    testset = np.genfromtxt(f'mnist_test.csv', delimiter=',', skip_header=2)
     arch = [(784, 50), (50, 30), (30, 30), (30, 20), (20, 12), (12, 10)]
 
     layers, biases = train(dataset, arch, 784, 10)
+    test(layers, biases, testset)
 
     # output_nn(layers, biases)
 
